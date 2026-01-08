ab7dc475cdba56a5e85307d8462f9f0b27937452
realorko <realorko@nowhere>
2026-01-08 17:57:24 +0000

    Refactored make files and config names

--- src/gcc_backend.c
+++ src/gcc_backend.c
@@ -165,10 +165,10 @@ static const char *get_config_filename(BackendType backend)
 {
     switch (backend)
     {
-        case BACKEND_CLANG:  return "sn.clang.cfg";
-        case BACKEND_TINYCC: return "sn.tcc.cfg";
+        case BACKEND_CLANG:  return "sn-clang.cfg";
+        case BACKEND_TINYCC: return "sn-tcc.cfg";
         case BACKEND_GCC:
-        default:             return "sn.gcc.cfg";
+        default:             return "sn-gcc.cfg";
     }
 }
 
